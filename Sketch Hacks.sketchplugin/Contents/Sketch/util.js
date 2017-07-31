62baf521edcf25c3a3a2833cc45d7d53c8339f2c
Tim Dosé <tim.dose@zocdoc.com>
2017-07-31 17:33:23 -0400

    fixed re-creating the link underline (missed util file)

--- Sketch Hacks.sketchplugin/Contents/Sketch/util.js
+++ Sketch Hacks.sketchplugin/Contents/Sketch/util.js
@@ -10,6 +10,31 @@ var getBaseline = function (fontSize, lineHeight) {
 	return Math.ceil((fontSize + lineHeight)/2);
 }
 
+com.timdose.sketch = {
+	sendAction: function (commandToPerform) {
+	    try {
+	        [NSApp sendAction:commandToPerform to:nil from:com.timdose.doc];
+	    } catch(e) {
+	        log(e)
+	    }
+	}
+}
+
+com.timdose.arrange = {
+	sendToBack: function () {
+	    com.timdose.sketch.sendAction('moveToBack:');
+	},
+	sendBackward: function () {
+	    com.timdose.sketch.sendAction('moveBackward:');
+	},
+	sendToFront: function () {
+	    com.timdose.sketch.sendAction('moveToFront:');
+	},
+	sendForward: function () {
+	  com.timdose.sketch.sendAction('moveForward:');
+	},
+}
+
 com.timdose.selection = {
 	deselectAll: function (page) {
 		if (MSApplicationMetadata.metadata().appVersion < 45) {
