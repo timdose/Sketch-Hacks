7a5351f8c4c47bf9daaa215e7ad5f787e6946185
Tim Dosé <tim.dose@zocdoc.com>
2017-08-25 10:10:04 -0400

    appcast + start of insta-detach

--- Sketch Hacks.sketchplugin/Contents/Sketch/Insta-Detach.js
+++ Sketch Hacks.sketchplugin/Contents/Sketch/Insta-Detach.js
@@ -4,9 +4,30 @@ var onRun = function( context ) {
 
 
 var onInsertSymbolComplete = function( context ) {
-	context.actionContext.document.showMessage('Symbol inserted!');
+	if ( context.actionContext.name == 'InsertSymbol' ) {
+		log( '\n\n\n----------------------------------------')
+		log( 'symbol placed');
+		log( '\n\ncontext\n--------------------\n' );
+		log( context );
+		log( '\n\ncontext.api\n--------------------\n' );
+		log( context.api() );
+		log( '\n\ncontext.api().selectedDocument\n--------------------\n' );
+		log( context.api().selectedDocument );
+		log( '\n\ncontext.api().selectedDocument.selectedLayers\n--------------------\n' );
+		log( context.api().selectedDocument.selectedLayers );
+	}
 }
 
 var onLayersMoved = function( context ) {
 	context.actionContext.document.showMessage('Layers moved!');
+}
+
+var onAction = function( context ) {
+	log( '\n\naction triggered' );
+	log( context );
+	log( context.actionContext.name );
+
+	if ( context.actionContext.name == 'InsertSymbol' ) {
+		log( '*********PLACED?!?!');
+	}
 }
